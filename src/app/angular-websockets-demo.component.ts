336239fdba063f1593c8c207d6d38438a488dc28
JavaFXpert <james@j1w.xyz>
2016-07-26 21:42:22 -0400

    Implement RegressionSum example

--- src/app/angular-websockets-demo.component.ts
+++ src/app/angular-websockets-demo.component.ts
@@ -251,10 +251,17 @@ export class AngularWebsocketsDemoAppComponent {
 
   // TODO: Work out this functionality
   updateScoreGraph(results: any) {
-    var items = [];
+    var items = [
+      {x: '2014-06-11', y: 10},
+      {x: '2014-06-12', y: 25},
+      {x: '2014-06-13', y: 30},
+      {x: '2014-06-14', y: 10},
+      {x: '2014-06-15', y: 15},
+      {x: '2014-06-16', y: 30}
+    ];
 
     var dataset = new vis.DataSet(items);
     var options = {};
-    this.graph2d = new vis.Graph2d(this.div.nativeElement, dataset, options);
+    this.graph2d = new vis.Graph2d(this.scoreGraph.nativeElement, dataset, options);
   }
 }
